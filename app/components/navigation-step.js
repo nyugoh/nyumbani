6b4f1e90f483c17c4b381cfff131c1d17eb4c76b
nyugoh <nyugoh@gmail.com>
2018-07-18 22:11:42 +0300

    Added a navigation step guide

---
+++ app/components/navigation-step.js
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+import { inject } from '@ember/controller';
+
+export default Component.extend({
+  // addController: inject('rentals.add'),
+  // setupController(model, controller) {
+  //   let page = addController.get('page');
+  //   console.log(page);
+  // }
+});
