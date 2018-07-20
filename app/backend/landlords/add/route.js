b02c955d479425c54028a6cd3c6d4654a2d5ed77
nyugoh <nyugoh@gmail.com>
2018-07-20 08:43:06 +0300

    Added landlords page

--- app/backend/landlords/add/route.js
+++ app/backend/landlords/add/route.js
@@ -1,4 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  setupController(controller, model) {
+    controller.set('model', this.store.createRecord('landlord'));
+  }
 });
--- app/backend/route.js
+++ app/backend/route.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  actions: {
+    saveLocation() {
+      let controller = this.controllerFor('backend.locations.add');
+      let model = controller.get('model');
+      model.save().then(location => {
+        controller.transitionToRoute('backend.locations');
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    saveLandlord() {
+      let controller = this.controllerFor('backend.landlords.add');
+      let model = controller.get('model');
+      model.save().then(landlord => {
+        controller.transitionToRoute('backend.landlords');
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  }
 });
